732cec1e0b70a64dd617d09772bb310c29287c77
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-13 13:38:28 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace learning_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input new string:");
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+        }
+    }
+}
