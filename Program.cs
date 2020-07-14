b7e87726ebca8b2913b47e30f2aef910418a51f6
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-13 17:00:39 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,11 +10,24 @@ namespace learning_csharp
             string a = Console.ReadLine();
             Console.Write("Enter Age: ");
             string b = Console.ReadLine();
-            Console.Write("Enter favorite color: ");
+            Console.Write("Enter your birth month: ");
             string c = Console.ReadLine();
             Console.WriteLine("Your name is  {0}", a);
             Console.WriteLine("Your age is {0}",b);
-            Console.WriteLine("Your favorite coloe is {0}", c);
+            Console.WriteLine("Your birth month is {0}", c);
+
+            if(c == "march")
+            {
+                Console.WriteLine("You are an Aries.");
+            }
+            else if(c == "april")
+                {
+                Console.WriteLine("You are a Taurus");
+            }
+            else if(c == "may")
+            {
+                Console.WriteLine("You are a Gemini");
+            }
         }
     }
 }
