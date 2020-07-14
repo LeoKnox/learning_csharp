bd91debdadca32c8b52d5970eff8d9a41c3692a3
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-14 08:23:47 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,14 +10,13 @@ namespace learning_csharp
             string u = Console.ReadLine();
             Console.Write("Enter Passcode: ");
             string p = Console.ReadLine();
-            if (p == "secret")
-            {
-                Console.WriteLine("User {0} has been authenticade.", u);
-            }
-            else
+            while (p != "secret")
             {
                 Console.WriteLine("User {0} cannot be Authenticated.", u);
+                Console.Write("Enter Passcode: ");
+                p = Console.ReadLine();
             }
+            Console.WriteLine("User {0} has been authenticade.", u);
         }
     }
 }
