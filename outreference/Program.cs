08062e195b0719767aeca52cf5c8d8656b312190
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-26 16:27:32 -0700

    Add files via upload

--- outreference/Program.cs
+++ outreference/Program.cs
@@ -1,1 +1,32 @@
-a
+﻿using System;
+
+namespace outreference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            Console.WriteLine(x);
+            Five(out x);
+            Console.WriteLine(x);
+        }
+
+        static void Five(out int a) // cannot overload out and ref though
+        {
+            a = 5;
+            Console.WriteLine(a);
+        }
+
+        /*static void Five(ref int a)
+        {
+            a = 5;
+            Console.WriteLine(a);
+        }*/
+        static void Five(int a) // you can overload function for reference and nonreference
+        {
+            a = 5;
+            Console.WriteLine(a);
+        }
+    }
+}
