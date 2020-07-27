5a8caf2a37eb70280e1eea2a8499a37ec02b0491
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-27 16:44:25 -0700

    Add files via upload

--- Console.cs
+++ Console.cs
@@ -12,5 +12,24 @@ namespace Util
             System.Console.Write(question); // System.Console is used here because this is Console.cs
             return System.Console.ReadLine();
         }
+
+        static public string Ask(int question)
+        {
+            System.Console.Write(question); // System.Console is used here because this is Console.cs
+            return System.Console.ReadLine();
+        }
+
+        static public int AskInt(string question)
+        {
+            try
+            {
+                System.Console.Write(question);
+                return int.Parse(System.Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                throw new FormatException("Input was not a number.");
+            }
+        }
     }
 }
