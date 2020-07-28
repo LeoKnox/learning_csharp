58d02c42b84b208c9b43fe6791b5cd17f5bb6ca6
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-28 10:07:04 -0700

    Add files via upload

--- Boxes/Program.cs
+++ Boxes/Program.cs
@@ -1,1 +1,46 @@
-a
+﻿using System;
+
+namespace Boxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sq = new Square() { Width = 2};
+            var tr = new Triangle() { Base = 2, Height = 5 };
+
+            sq.Display();
+            tr.Display();
+        }
+    }
+
+    abstract class Shape
+    {
+        public abstract int GetArea();
+
+        public void Display()
+        {
+            Console.WriteLine("The area is {0}", GetArea());
+        }
+    }
+    class Square : Shape
+    {
+        public int Width;
+
+        public override int GetArea()
+        {
+            return Width * Width;
+        }
+    }
+    
+    class Triangle : Shape
+    {
+        public int Base;
+        
+        public override int GetArea()
+        {
+            return (Base * Height) / 2;
+        }
+        public int Height;
+    }
+}
