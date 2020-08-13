441ea9547767c5b4bab88bdd74c3ce86d2d1b7c4
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-12 17:27:20 -0700

    Update sum.cs

--- sum.cs
+++ sum.cs
@@ -5,11 +5,18 @@ public class Program
 	public static int sumup(int i)
 	{
 		int sum = 0;
+		while (i > 9)
+		{
+			sum += i%10;
+			i /= 10;
+			//Console.WriteLine("i = {0}", i);
+		}
+		sum += i;
 		return(sum);
 	}
 	public static void Main()
 	{
-		int[] nums = {3, 13, 32, 51, 124, 2311};
+		int[] nums = {3, 13, 32, 51, 124, 2312};
 		foreach (int n in nums)
 		{
 			Console.WriteLine(sumup(n));
